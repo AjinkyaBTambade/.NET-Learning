6d86d87ca5dbea2dfaf2d5f13bb5c7b62ea4fa9b
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-25 13:53:44 +0530

    Simple WebAppWithRazorPages

---
+++ WebAppWithRazor/WebAppWithRazor/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppWithRazor.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
