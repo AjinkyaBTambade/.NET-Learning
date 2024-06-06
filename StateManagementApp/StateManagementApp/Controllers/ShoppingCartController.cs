3cfe8ef7aa15f639ca722cb873c45698f3f2ab88
Ajinkya <ajinkyatambade143@gmail.com>
2024-06-06 22:39:52 +0530

    State Management Functionality Added

---
+++ StateManagementApp/StateManagementApp/Controllers/ShoppingCartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateManagementApp.Models; // Ensure this is pointing to the correct namespace
+using StateManagmentApp.Models;
+using System.Text.Json;
+
+namespace StateManagementApp.Controllers
+{
+    // Server Side State Management
+    // Session Management
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            Cart theCart = new Cart();
+            theCart.Items.Add("laptop");
+            string json = JsonSerializer.Serialize(theCart);
+            TempData["mycart"] = json;
+            return RedirectToAction("RemoveFromCart");
+        }
+
+        // session is essential when data is supposed to be accessed
+        // across any action method belonging to any controller
+        public IActionResult RemoveFromCart()
+        {
+            if (TempData["mycart"] is string json)
+            {
+                Cart existingCart = JsonSerializer.Deserialize<Cart>(json);
+                existingCart.Items.Remove("laptop");
+                ViewData["mycart"] = existingCart;
+            }
+            return View();
+        }
+    }
+}
---
+++ StateManagementApp/StateManagementApp/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿namespace StateManagmentApp.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public List<string> Items { get; set; }
+        public Cart()
+        {
+            this.Items = new List<string>();
+
+        }
+    }
+}
