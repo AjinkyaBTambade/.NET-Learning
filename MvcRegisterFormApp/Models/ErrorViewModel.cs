54e68fb5edf4e5de22d3470e092913c5a1eff31e
Ajinkya <ajinkyatambade143@gmail.com>
2024-06-07 19:15:01 +0530

    State Management

--- MvcRegisterFormApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcRegisterFormApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
