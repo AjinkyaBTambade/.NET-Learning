156d5a15bc33aa1a843b738e792e7339ad1782a9
Ajinkya <ajinkyatambade143@gmail.com>
2024-06-10 00:31:31 +0530

    CHanges

---
+++ StateManagementApp/StateManagmentApp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StateManagmentApp.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
